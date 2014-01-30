5f8ac009caa51ad961ad1a6a233c7f939ccca78f
ysfj <tgweb12@yahoo.co.jp>
2014-01-31 06:31:31 +0900

    Update TTSite.pm

--- lib/Umakatter/Web/View/TTSite.pm
+++ lib/Umakatter/Web/View/TTSite.pm
@@ -4,28 +4,6 @@ use namespace::autoclean;
 
 extends 'Catalyst::View';
 
-=head1 NAME
-
-Umakatter::Web::View::TTSite - Catalyst View
-
-=head1 DESCRIPTION
-
-Catalyst View.
-
-
-=encoding utf8
-
-=head1 AUTHOR
-
-末藤 悠
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
 __PACKAGE__->meta->make_immutable;
 
 1;
